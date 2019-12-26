8fba2f500bc33ff7b8fd0c289654dd9376c1f6a9
ckbhatia <chetansain86@gmail.com>
2019-12-26 21:42:30 +0530

    feat: add state, useEffect and pass to header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import "bootstrap/dist/css/bootstrap.css"
+// import "bootstrap/dist/css/bootstrap-theme.css"
+import "../assets/main.scss"
+import Header from "../components/Header"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  const [isDay, toggleDay] = useState(true)
+
+  useEffect(() => {
+    if (!isDay) {
+      document.body.style.backgroundColor = "black"
+    }
+    return () => {
+      document.body.style.backgroundColor = "white"
+    }
+  }, [isDay])
+
+  return (
+    <div className={`main-app ${isDay ? "day" : "dark"}`}>
+      <Header isDay={isDay} toggleDay={toggleDay} />
+    </div>
+  )
+}
