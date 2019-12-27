22d3f8b7b251c2dde4343ae0efdf8ce712ea1271
ckbhatia <chetansain86@gmail.com>
2019-12-27 20:11:33 +0530

    feat: wrap layout component with context provider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from "react"
-import "bootstrap/dist/css/bootstrap.css"
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 // import "bootstrap/dist/css/bootstrap-theme.css"
-import "../assets/main.scss"
-import Header from "../components/Header"
+import "../assets/main.scss";
+import Layout from "../components/Layout";
+
+import Context from "../context";
 
 export default () => {
-  const [isDay, toggleDay] = useState(true)
+  const [isDay, toggleDay] = useState(true);
 
   useEffect(() => {
     if (!isDay) {
-      document.body.style.backgroundColor = "black"
+      document.body.style.backgroundColor = "black";
     }
     return () => {
-      document.body.style.backgroundColor = "white"
-    }
-  }, [isDay])
+      document.body.style.backgroundColor = "white";
+    };
+  }, [isDay]);
 
   return (
     <div className={`main-app ${isDay ? "day" : "dark"}`}>
-      <Header isDay={isDay} toggleDay={toggleDay} />
+      <Context.Provider value={{ isDay, toggleDay }}>
+        <Layout>{/* Content or components */}</Layout>
+      </Context.Provider>
     </div>
-  )
-}
+  );
+};
