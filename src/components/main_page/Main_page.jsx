9b176cfefe85d49a023bd629105335d216853c1a
ckbhatia <chetansain86@gmail.com>
2020-01-09 17:02:44 +0530

    feat: import articles and pass it to return

--- src/components/main_page/Main_page.jsx
+++ src/components/main_page/Main_page.jsx
@@ -5,31 +5,43 @@ import Loader from "../Loader";
 
 const Latestpost = lazy(() => import("./Latest_post"));
 const SideBar = lazy(() => import("./Sidebar"));
+const Articles = lazy(() => import("./Articles"));
 
 export default function MainPage() {
   return (
-    <Div className="main-page-main-container wrapper">
+    <Section className="main-page-main-container wrapper">
       <Suspense fallback={<Loader />}>
-        <Latestpost />
-        <SideBar />
+        <section className="hero-section">
+          <Latestpost />
+          <SideBar />
+        </section>
+        <section className="articles-section">
+          <Articles />
+        </section>
       </Suspense>
-    </Div>
+    </Section>
   );
 }
 
-const Div = styled.div`
-  display: grid;
-  grid-template-columns: 65% 30%;
-  grid-gap: 4rem;
-  align-items: center;
-  @media screen and (max-width: 992px) {
-    grid-template-columns: 57%;
-    justify-content: center;
+const Section = styled.section`
+  .hero-section {
+    display: grid;
+    grid-template-columns: 65% 30%;
+    grid-gap: 4rem;
+    align-items: center;
+    @media screen and (max-width: 992px) {
+      // grid-template-columns: 57%;
+      grid-template-columns: 70%;
+      justify-content: center;
+    }
+    @media screen and (max-width: 768px) {
+      grid-template-columns: 68%;
+    }
+    @media screen and (max-width: 600px) {
+      grid-template-columns: 88%;
+    }
   }
-  @media screen and (max-width: 768px) {
-    grid-template-columns: 68%;
-  }
-  @media screen and (max-width: 600px) {
-    grid-template-columns: 88%;
+  .articles-section {
+    margin-top: 4rem;
   }
 `;
