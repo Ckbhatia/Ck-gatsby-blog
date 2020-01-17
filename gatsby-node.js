691acb641fe2ef50d2be32da33db96493ab6a73b
ckbhatia <chetansain86@gmail.com>
2020-01-17 19:07:45 +0530

    feat: add createPages node api

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,3 +11,31 @@ module.exports.onCreateNode = ({ node, actions }) => {
     });
   }
 };
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const blogTemplate = path.resolve("./src/templates/blog.js");
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    });
+  });
+};
