e225110e10c1ad9323afbfcc00cfcf6e75376f67
ckbhatia <chetansain86@gmail.com>
2020-01-29 21:05:43 +0530

    feat: add fetch dynamic data functionality

--- src/components/main_page/Latest_post.jsx
+++ src/components/main_page/Latest_post.jsx
@@ -1,50 +1,74 @@
 import React, { useContext } from "react";
 import Context from "../../context";
+import { Link, graphql, useStaticQuery } from "gatsby";
 import styled from "styled-components";
-import "../../assets/main.scss";
 
 export default function Latestpost() {
   const { isDay } = useContext(Context);
 
+  const data = useStaticQuery(graphql`
+    {
+      allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        limit: 1
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+              topic
+            }
+            html
+            excerpt
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const { title, topic } = data.allMarkdownRemark.edges[0].node.frontmatter;
+  const slug = data.allMarkdownRemark.edges[0].node.fields.slug;
   return (
-    <Article className="latest-post-main-container">
-      {/* TODO: Add dynamic url */}
-      <a href="#" className="latest-post-link">
-        <div
-          className={`latest-post-container container-bg-${
-            isDay ? "light" : "dark"
-          }`}
-        >
-          <div className="latest-post-img-container">
-            {/* TODO: Add dynamic image url */}
-            <img
-              src="https://tinyurl.com/smxee9n"
-              alt="latest-post"
-              className="latest-post-img"
-            />
+    <>
+      <Article className="latest-post-main-container">
+        <Link to={`/blog/${slug}`} className="latest-post-link">
+          <div
+            className={`latest-post-container container-bg-${
+              isDay ? "light" : "dark"
+            }`}
+          >
+            <div className="latest-post-img-container">
+              {/* TODO: Add dynamic image url */}
+              <img
+                src="https://tinyurl.com/smxee9n"
+                alt="latest-post"
+                className="latest-post-img"
+              />
+            </div>
+            <div className="latest-post-header-container">
+              <span
+                className={`latest-post-header-topic-cate-text text-${
+                  isDay ? "light" : "dark"
+                }`}
+              >
+                {topic}
+              </span>
+              <h3
+                className={`latest-post-heading heading-${
+                  isDay ? "light" : "dark"
+                }`}
+              >
+                {title}
+              </h3>
+            </div>
           </div>
-          <div className="latest-post-header-container">
-            <span
-              // Note: cate stands for category
-              className={`latest-post-header-topic-cate-text text-${
-                isDay ? "light" : "dark"
-              }`}
-            >
-              {/* TODO: Add dynamic topic category */}
-              TECHNOLOGY
-            </span>
-            <h3
-              className={`latest-post-heading heading-${
-                isDay ? "light" : "dark"
-              }`}
-            >
-              {/* TODO: Add dynamic topic category */}
-              Are you ready to take a deep dive into the web in 2020?
-            </h3>
-          </div>
-        </div>
-      </a>
-    </Article>
+        </Link>
+      </Article>
+    </>
   );
 }
 
@@ -84,8 +108,14 @@ const Article = styled.article`
       margin: 1rem 2rem 0 2rem;
       padding: 0.4rem 0 1.6rem 0;
       .latest-post-header-topic-cate-text {
-        font-size: 0.7rem;
+        font-size: 1rem;
         font-weight: 700;
+      }
+      .text-light {
+        color: #343a40 !important;
+      }
+      .text-dark {
+        color: rgba(245, 245, 245, 0.9) !important;
       }
       .latest-post-heading {
         font-size: 1.6rem;
