e54a5d42be9804dff8f0d0e2b5a20fbe6aedb3d7
ckbhatia <chetansain86@gmail.com>
2020-01-31 23:25:30 +0530

    feat: configure source contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
     author: "Chetan Kumar",
   },
   plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
