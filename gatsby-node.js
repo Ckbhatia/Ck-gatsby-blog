70e2f06f1d5027acf4cb64f678e0d661e0246d41
ckbhatia <chetansain86@gmail.com>
2020-02-07 22:00:13 +0530

    fix: replace createpages query & del onCreateNode

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,40 +1,26 @@
 const path = require("path");
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions;
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md");
-    createNodeField({
-      node,
-      name: "slug",
-      value: slug,
-    });
-  }
-};
-
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
   const blogTemplate = path.resolve("./src/templates/blog.js");
   const res = await graphql(`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
     }
   `);
 
-  res.data.allMarkdownRemark.edges.forEach(edge => {
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
+      path: `/blog/${edge.node.slug}`,
       context: {
-        slug: edge.node.fields.slug,
+        slug: edge.node.slug,
       },
     });
   });
