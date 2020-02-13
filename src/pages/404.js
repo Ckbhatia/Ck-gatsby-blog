9bdf6e1c3666404df09b676087e074661ed9b0a0
ckbhatia <chetansain86@gmail.com>
2020-02-13 18:26:14 +0530

    feat: create 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Mode from "../components/Mode";
+import { Link } from "gatsby";
+import Head from "../components/Head";
+import Layout from "../components/Layout";
+import styled from "styled-components";
+
+export default function PageNotFound() {
+  return (
+    <Mode>
+      <Layout>
+        <Head title="404" />
+        <Div className="page-not-found-main center-child">
+          <h1 className="page-not-found-heading">404 Page not found</h1>
+        </Div>
+      </Layout>
+    </Mode>
+  );
+}
+
+const Div = styled.div`
+  height: 79vh;
+  a {
+    display: block;
+  }
+`;
